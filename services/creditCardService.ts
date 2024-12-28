9e9aee880b3c80e7d9f8c68f3f66444b2eb5ba6d
iremnurmemis <iremnurmemis123@gmail.com>
2024-12-28 22:41:14 +0300

    service

--- services/creditCardService.ts
+++ services/creditCardService.ts
@@ -8,7 +8,19 @@ export interface CreditCard{
 export const CreditCardService={
 
  
-  
+   getUserCards: async (userId: number): Promise<CreditCard[]> => {
+    try {
+      const response = await axios.get(`http://localhost:5153/api/Cards/getUserCards`, {
+        params: { userId },
+      });
+      return response.data.data; // API'den dönen araç konumlarını al
+    } catch (error) {
+      console.error("Userın kartları gelirken hata oluştu:", error);
+      throw error;
+    }
+  },
+
+
 
 };
 
